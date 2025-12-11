c223b79318dbf27584c699b310aaf8c59882c564
manuvaladares <manuella.valadares@hotmail.com>
2025-12-11 13:28:03 -0300

    feat: adiciona interfaces de itens específicos e o TypeUnion de ItemLeilao

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,27 @@ enum TipoItem {Casa, Carro, ItemDeValor};
 interface Item{
     id: number;
     nome: string;
-    PrecoInicial: number;
+    precoInicial: number;
     tipo: TipoItem;
-};
+}
 
 interface Carro extends Item {
     ano: number;
     modelo: string;
     automatico?: boolean;
-};
+    tipo: TipoItem.Carro;
+}
+
+interface Casa extends Item {
+    endereco: string;
+    area_m2: number;
+    tipo: TipoItem.Casa;
+}
+
+interface ItemDeValor extends Item {
+    material: string;
+    marca: string;
+    tipo: TipoItem.ItemDeValor;
+}
+
+type itemLeilao = Casa | Carro | ItemDeValor;
