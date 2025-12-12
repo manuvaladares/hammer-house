cfb12ae7a57ad2e0dcf3854a43e1431e4a6350ed
manuvaladares <manuella.valadares@hotmail.com>
2025-12-12 11:03:45 -0300

    feat: instancia um leilão e um item dentro da main

--- src/index.ts
+++ src/index.ts
@@ -72,3 +72,19 @@ class SistemaLeilao {
         console.log("Lance aceito.");
     }
 }
+
+async function main() {
+    const leilao = new SistemaLeilao();
+
+    const mustang: Carro = {
+        id: 1,
+        nome: "Mustang 1969",
+        precoInicial: 50000,
+        tipo: TipoItem.Carro,
+        ano: 1969,
+        modelo: "Mustang 1969",
+        automatico: false
+    }
+
+    await leilao.cadastraItem(mustang);
+}
