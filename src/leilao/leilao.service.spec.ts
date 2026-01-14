753728c18b94c6f64dab8aaeafea99f2ec9fb24c
Manuella <manuella.valadares@hotmail.com>
2026-01-14 19:20:15 -0300

    feat: tests

--- src/leilao/leilao.service.spec.ts
+++ src/leilao/leilao.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LeilaoService } from './leilao.service';
+import { CreateLeilaoDto } from './dto/create-leilao.dto';
 
 describe('LeilaoService', () => {
   let service: LeilaoService;
@@ -14,5 +15,53 @@ describe('LeilaoService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('deve criar um leilão corretamente', () => {
+    const dto: CreateLeilaoDto = {
+      nome: 'Violão Gibson',
+      precoInicial: 5000,
+      usado: true,
+    };
+
+    const resultado = service.create(dto);
+
+    expect(resultado).toHaveProperty('id');
+    expect(resultado.nome).toBe('Violão Gibson');
+    
+    const todos = service.findAll();
+    expect(todos).toHaveLength(1);
+  });
+
+  it('deve aceitar um lance válido', () => {
+    const leilao = service.create({
+      nome: 'TV Antiga',
+      precoInicial: 100,
+      usado: true,
+    });
+
+    const mensagem = service.darLance({
+      usuario: 'Comprador1',
+      valor: 200, 
+      leilaoId: leilao.id,
+    });
+
+    expect(mensagem).toBe('Lance aceito!');
+  });
+
+  it('deve rejeitar um lance menor que o preço inicial', () => {
+    const leilao = service.create({
+      nome: 'TV Antiga',
+      precoInicial: 100,
+      usado: true,
+    });
+
+    expect(() => {
+      service.darLance({
+        usuario: 'Fulano',
+        valor: 50,
+        leilaoId: leilao.id,
+      });
+    }).toThrow('Lance rejeitado: R$50 é menor que o mínimo.');
   });
 });
--- src/leilao/leilao.service.ts
+++ src/leilao/leilao.service.ts
@@ -27,7 +27,7 @@ export class LeilaoService {
   }
 
   findAll() {
-    return `This action returns all leilao`;
+    return this.leiloes;
   }
 
   darLance(lance: Lance) { 
